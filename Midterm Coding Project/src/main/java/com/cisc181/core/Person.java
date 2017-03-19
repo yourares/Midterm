d71736e55fd0ada623184808e3d87bf92a59ec2f
appleud <appleud@192.168.0.107>
2017-03-19 15:51:53 -0400

    change

--- Midterm Coding Project/src/main/java/com/cisc181/core/Person.java
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Person.java
@@ -56,39 +56,6 @@ public abstract class Person implements java.io.Serializable {
 		this.DOB = DOB;
 
 	}
-	
-	public void setDOB2(Date DOB) throws PersonException {
-		
-		Calendar today = Calendar.getInstance();
-		Calendar birthDate = Calendar.getInstance();
-
-		int age = 0;
-		birthDate.setTime(DOB);
-		if (birthDate.after(today)) {
-			throw new IllegalArgumentException("Can't be born in the future");
-		}
-		age = today.get(Calendar.YEAR) - birthDate.get(Calendar.YEAR);
-
-		// If birth date is greater than todays date (after 2 days adjustment of
-		// leap year) then decrement age one year
-		if ((birthDate.get(Calendar.DAY_OF_YEAR)
-				- today.get(Calendar.DAY_OF_YEAR) > 3)
-				|| (birthDate.get(Calendar.MONTH) > today.get(Calendar.MONTH))) {
-			age--;
-
-			// If birth date and todays date are of same month and birth day of
-			// month is greater than todays day of month then decrement age
-		} else if ((birthDate.get(Calendar.MONTH) == today.get(Calendar.MONTH))
-				&& (birthDate.get(Calendar.DAY_OF_MONTH) > today
-						.get(Calendar.DAY_OF_MONTH))) {
-			age--;
-		}
-
-		if (age < 100)
-			this.DOB = DOB;
-		else
-			throw new PersonException(this);
-}
 
 	public void setAddress(String newAddress) {
 		address = newAddress;
